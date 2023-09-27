22197fb1e5ddb272badf143bd5c9c0c974791a05
yeasir10967 <145603405+yeasir10967@users.noreply.github.com>
2023-09-27 10:59:35 +0600

    A+B?

---
+++ 1772A.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        char a[5];
+    scanf("%s",a);
+     printf("%d\n",a[0]+a[2]-96);
+     }
+    return 0;
+}
