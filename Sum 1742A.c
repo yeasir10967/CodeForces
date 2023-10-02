2a53301dbeda1337676338ce9da5fa55f2ac8c64
yeasir10967 <145603405+yeasir10967@users.noreply.github.com>
2023-10-02 12:12:57 +0600

    Create Sum 1742A

---
+++ Sum 1742A.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        if (a + b == c || b + c == a || c + a == b)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+
+    return 0;
+}
