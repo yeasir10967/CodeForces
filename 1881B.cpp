1cb037e215139f4c60e9c069bb46ff4e371cc61b
MD Yeasir Arafat <145603405+yeasir10967@users.noreply.github.com>
2024-03-12 22:42:08 +0600

    Add files via upload

---
+++ 1881B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        int arr[3] = {a, b, c};
+        sort(arr, arr + 3);
+        a = arr[0];
+        b = arr[1];
+        c = arr[2];
+        
+        if (a == b && b == c) {
+            cout << "YES" << endl;
+        } 
+        else if (b % a == 0 && c % a == 0 && (b / a - 1) + (c / a - 1) <= 3) {
+            cout << "YES" << endl;
+        } 
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
+
+
+
