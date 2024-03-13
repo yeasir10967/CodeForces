9d3d41db995db405dcffd20f7faf516ee5a0aae5
MD Yeasir Arafat <145603405+yeasir10967@users.noreply.github.com>
2024-03-13 12:51:57 +0600

    Update 1929B.cpp

--- 1929B.cpp
+++ 1929B.cpp
@@ -7,12 +7,15 @@ int main()
     while(t--)
     {
         int n,k;
-           cin>>n>>k;
+        cin>>n>>k;
         if(k==4*n-2)
         {
             cout<<2*n<<"\n";
         }
-        cout<<(k+1)/2<<"\n";
+        else 
+        {
+            cout<<(k+1)/2<<"\n";
+        }
     }
 
     return 0;
