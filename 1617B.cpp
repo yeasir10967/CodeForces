ecf48ab915e205475b01ef378608e797822b8535
MD Yeasir Arafat <145603405+yeasir10967@users.noreply.github.com>
2024-03-14 14:00:55 +0600

    Add files via upload

---
+++ 1617B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a,b,c;
+        for(a=2,b=n-3;__gcd(a,b)!=1;a++,b--)
+        {
+
+        }
+        cout<<a<<' '<<b<<' '<<1<<"\n";
+
+    }
+    return 0;
+}
+
+
