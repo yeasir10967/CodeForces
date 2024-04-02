94d91cbf7ab2555d26b24dc2f55e23d99f4042c8
MD Yeasir Arafat <145603405+yeasir10967@users.noreply.github.com>
2024-04-02 23:21:01 +0600

    Add files via upload

---
+++ 1952C.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int n; 
+    cin >> n;
+    vector<int> arr={10, 10, 8, 9, 8, 7, 7, 7, 7, 0, 9, 6, 8};
+    
+    cout << arr[n] <<endl;
+    return 0;
+}
