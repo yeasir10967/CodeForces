498a2fcafe45440e6e3f0663d07deee258e635da
MD Yeasir Arafat <145603405+yeasir10967@users.noreply.github.com>
2024-04-02 23:00:09 +0600

    Add files via upload

---
+++ 1952A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main()
+{
+     optimize();
+     cout<<"safety"<<endl;
+    return 0;
+}
